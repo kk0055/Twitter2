eaa2db02ed00b14b60d7e5b4de5af5f0a6b74e87
kk0055 <rtsw.lgy@gmail.com>
2020-11-02 10:33:29 +0800

    Seeder

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,5 +11,5 @@ class Favorite extends Model
     public $timestamps = false;
 
      //tweetにbelongsToはいらないのか？
-     
+
 }
