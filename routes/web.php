df31b7c7df61228858cc5bdb88ba18fe5a3556ff
kk0055 <rtsw.lgy@gmail.com>
2020-11-04 15:16:20 +0800

    Tweet index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\TweetsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,9 +28,11 @@ Route::group(['middleware' => 'auth'], function() {
     Route::resource('users',UsersController::class,
    ['only'=> ['index','show','edit','update']]);
 
-
+  
+   
    Route::post('users/{user}/follow', [UsersController::class,'follow'])->name('follow');
    Route::delete('users/{user}/unfollow', [UsersController::class,'unfollow'])->name('unfollow');  
-
+ 
+   Route::resource('tweets',TweetsController::class, ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
 
 });
